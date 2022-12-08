4de9b522f9d3371ea4d48184def2d27e86b37f03
исп-31 <-31@LAB44-05>
2022-12-08 13:21:56 +0300

    Добавьте файлы проекта.

---
+++ Pract8/GeometricProgression.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Controls;
+using System.Xml.Linq;
+
+namespace Pract8
+{
+    internal class GeometricProgression : ISeries
+    {
+        private int _firstValue, _secondValue;
+        private double _step;
+        public int FirstValue
+        {
+            get { return _firstValue; }
+            set { _firstValue = value; }
+        }
+        public int SecondValue
+        {
+            get { return _secondValue; }
+            set { _secondValue = value; }
+        }
+        public double Step
+        {
+            get { return _step; }
+            set { _step = _secondValue / _firstValue; }
+        }
+        public GeometricProgression(int firstValue, int secondValue)
+        {
+            _firstValue = firstValue;
+            _secondValue = secondValue;
+        }
+        
+        public int CompareTo(object obj)
+        {
+            GeometricProgression progression = (GeometricProgression)obj;
+            double step1=progression.Step;
+            double step2=this.Step;
+            return step2.CompareTo(step1);
+        }
+       
+        public GeometricProgression ShallowClone()
+        {
+            return (GeometricProgression)MemberwiseClone();
+        }
+
+        public object Clone()
+        {
+            return new GeometricProgression(FirstValue,SecondValue);
+        }
+    }
+}
---
+++ Pract8/ISeries.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Pract8
+{
+    internal interface ISeries
+    {
+        int FirstValue { get; set; }
+        int SecondValue { get; set; }
+        double Step { get; set; }
+    }
+}
